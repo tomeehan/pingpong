4293d6c6d051f97b2d284b77253a006caf2cb2d6
tomeehan <tom.meehan@me.com>
2017-11-24 23:25:38 +0000

    resolve game loser

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -15,8 +15,13 @@ class MatchesController < ApplicationController
 
   def create
     @match = Match.new(match_params)
+    @opponent = User.find(params[:match][:opponent])
 
     if @match.save!
+      @match.games.each do |game|
+        game.update_attribute(:loser, resole_loser(game))
+      end
+
       redirect_to matches_path
     else
       render :new
@@ -26,6 +31,11 @@ class MatchesController < ApplicationController
   private 
 
   def match_params
-    params.require(:match).permit(:category_id, games_attributes: [:id, :winner, :loser])
+    params.require(:match).permit(:category_id, :opponent, games_attributes: [:id, :winner, :loser])
   end
+
+  def resole_loser(game)
+    game.winner == current_user.id ? @opponent.id : current_user.id
+  end
+  
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,6 @@ class Match < ApplicationRecord
   
   has_many :games 
   accepts_nested_attributes_for :games, reject_if: :all_blank, allow_destroy: true
+
+  attr_accessor :opponent
 end
