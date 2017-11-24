5b200e4fe69a69270ed9f7924145d2668ced4579
tomeehan <tom.meehan@me.com>
2017-11-24 22:06:13 +0000

    Category create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,20 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new
+
+    if @category.save!(category_params)
+      redirect_to matches_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :win, :loss, :victory)
+  end
 end
