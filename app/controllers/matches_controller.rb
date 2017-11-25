368d24327613a172691c7762172e5bb8f730e9ba
tomeehan <tom.meehan@me.com>
2017-11-25 00:14:27 +0000

    refactor this

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -20,7 +20,8 @@ class MatchesController < ApplicationController
 
     if @match.save!
       create_participants
-      @match.games.each{ |game| game.update_attribute(:loser, resolve_loser(game)) }
+      @match.games.each{ |game| game.update_attribute(:loser, resolve_game_loser(game)) }
+      resolve_match_winner
       redirect_to matches_path
     else
       render :new
@@ -33,8 +34,23 @@ class MatchesController < ApplicationController
     params.require(:match).permit(:category_id, :opponent, games_attributes: [:id, :winner, :loser])
   end
 
-  def resolve_loser(game)
+  def resolve_game_loser(game)
     game.winner == current_user.id ? @opponent.id : current_user.id
+  end
+
+  def resolve_match_winner
+    if current_user_is_winner
+      winner, loser = current_user.id, @opponent.id
+    else
+      winner, loser = @opponent.id, current_user.id
+    end
+
+    @match.update_attribute(:winner, winner)
+    @match.update_attribute(:loser, loser)
+  end
+
+  def current_user_is_winner
+    @match.games.where(winner: current_user.id).size > @match.games.where(winner: @opponent.id).size
   end
 
   def create_participants
